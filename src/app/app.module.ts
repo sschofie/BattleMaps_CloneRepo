4bf82c702590823a43038db59554028a3525c23a
shardzog <35275593+shardzog@users.noreply.github.com>
2020-11-27 21:35:16 -0600

    feat: add map and randomization functionality (#108)
    * feat: add map and randomization functionality
    
    Added an image to the html with a button that randomizes an image. Since
    we don't currently have map images, I am using tiles as image
    placeholders.
    
    closes: #95
    closes: #96

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShowMapComponent } from './show-map/show-map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowMapComponent
   ],
   imports: [
     BrowserModule
