3ab5b5e89a190896e8c6c25a6607733233c5f965
shardzog <35275593+shardzog@users.noreply.github.com>
2021-02-02 09:53:23 -0600

    fix: add dwarf images (#143)
    * fix: add dwarf images
    
    - changed component.ts to randomize dwarf images instead of terrain
      pieces
    - changed break to use bootstrap mt-4 in class
    - map randomizes on init
    
    closes: #121

--- src/app/show-map/show-map.component.ts
+++ src/app/show-map/show-map.component.ts
@@ -7,19 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ShowMapComponent implements OnInit {
-  mapDisplay = 'assets/img/Terrain_Pieces_9.svg';
+  mapDisplay = '';
 
   constructor() { }
 
   ngOnInit() {
+    this.switchMap();
   }
 
   switchMap() {
-    const dwarfImages = Array('Terrain_Pieces_1.svg', 'Terrain_Pieces_2.svg', 'Terrain_Pieces_3.svg',
-      'Terrain_Pieces_4.svg', 'Terrain_Pieces_5.svg', 'Terrain_Pieces_6.svg', 'Terrain_Pieces_7.svg',
-      'Terrain_Pieces_8.svg', 'Terrain_Pieces_9.svg');
-    const chooseIndex =  Math.round(Math.random() * (dwarfImages.length - 1));
-    this.mapDisplay = 'assets/img/' + dwarfImages[chooseIndex];
+    const mapNum = 20;
+    const chooseIndex =  Math.round(Math.random() * (mapNum-1))+1;
+    this.mapDisplay = 'assets/img/maps/ed' +chooseIndex+'.svg';
   }
 
 }
