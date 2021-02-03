472940618a88e06f190356496550ebf95906d423
Morgen Halle <47124777+mhalle0@users.noreply.github.com>
2021-02-02 20:55:05 -0600

    chore: resolve exceed budget size error
    - Removed unused FontAwesome icon pack
    - increased component style budgets to 12kb (warning) and 16kb (error) (#151)
    
    Closes: #145

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -6,7 +6,6 @@ import { environment } from 'src/environments/environment';
   templateUrl: './landing-page.component.html',
   styleUrls: [
     './landing-page.component.css',
-    './vendor/fontawesome-free/css/all.min.css',
     './vendor/simple-line-icons/css/simple-line-icons.css'
   ]
 })
