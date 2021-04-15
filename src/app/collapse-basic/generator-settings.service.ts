c16bd03fcae3b4621684f1cdd46e05c9d3f55c11
shardzog <shardzog@trinity.edu>
2021-04-15 15:48:19 -0500

    feat: toggle hills
    - adds toggle to hills outside deployment zones
    - fixes tests to work with service
    
    closes: #251

--- src/app/collapse-basic/generator-settings.service.ts
+++ src/app/collapse-basic/generator-settings.service.ts
@@ -5,10 +5,15 @@ import { Injectable } from '@angular/core';
 })
 export class GeneratorSettingsService {
   public useEDMaps = true;
+  public hillNotInZones = true;
 
   constructor() { }
 
   toggleUseEDMaps() {
     this.useEDMaps = !this.useEDMaps;
+  }
+
+  toggleHillSettings() {
+    this.hillNotInZones = !this.hillNotInZones;
   }
 }
