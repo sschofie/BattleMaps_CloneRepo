d43377751d203ed21ee7b03ec3c4b78ebdf930aa
wyntrr-end <66278138+wyntrr-end@users.noreply.github.com>
2021-05-24 22:02:59 -0500

    fix: google analytics + angular (#287)
    * tags will only occur once per site load (or page reload), will not record in-app navigation
    * all visits will be recorded as arriving at /app or /about, depending on which page the user arrived at
    
    Closes: #261

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component} from '@angular/core';
-import { FeatureFlagsService } from './feature-flag/feature-flags.service';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +7,4 @@ import { FeatureFlagsService } from './feature-flag/feature-flags.service';
 })
 
 export class AppComponent{
-
 }
